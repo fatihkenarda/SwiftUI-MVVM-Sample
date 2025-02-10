0f8eef511051beb8d3b1c4780ad49869e22ff5c9
Fatih <fatihk.96brs@gmail.com>
2025-02-10 14:59:56 +0300

    Initial Commit

---
+++ MVVMProject/MVVMProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMProjectApp.swift
+//  MVVMProject
+//
+//  Created by Fatih Kenarda on 10.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct MVVMProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVVMProjectTests/MVVMProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMProjectTests.swift
+//  MVVMProjectTests
+//
+//  Created by Fatih Kenarda on 10.02.2025.
+//
+
+import Testing
+@testable import MVVMProject
+
+struct MVVMProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
